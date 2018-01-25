240f51cf22355db4857c54f0c9d7309a1d10c426
Xheartx <546593797@qq.com>
2018-01-25 22:12:50 +0800

    首次提交

---
+++ app.js
@@ -0,0 +1,88 @@
+/*
+ * @Author: X_Heart
+ * @Date: 2017-06-09 10:46:38
+ * @Last Modified by: X_Heart
+ * @Last Modified time: 2018-01-25 22:10:39
+ * @description: 
+ */
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const router = require('./router/router')
+const axios = require('axios')
+
+// 设置模板引擎
+app.engine('html', ejs.__express)
+app.set('view engine', 'html')
+
+app.set('port', (process.env.PORT || 5000));
+// 静态文件中间件
+app.use(express.static('./public'))
+
+var apiRoutes = express.Router()
+app.use('/api', apiRoutes)
+
+app.get('/api/getDiscList', (req, res) => {
+  var url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+app.get('/api/lyric', (req, res) => {
+  var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /^\w+\(({[^()]+})\)$/
+      var matches = ret.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+app.get('/api/disc', (req, res) => {
+  var url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /^\w+\(({[^()]+})\)$/
+      var matches = ret.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
